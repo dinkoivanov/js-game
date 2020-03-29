15af5848e2464a17f95667e70a45ddf3240101a5
Dinko Ivanov <dinkoi@vmware.com>
2020-03-30 00:24:51 +0300

    fixes

--- game.js
+++ game.js
@@ -4,18 +4,29 @@ const CELL_SIZE = 20;
 
 const CANVAS_WIDTH = H_CELLS * CELL_SIZE;
 const CANVAS_HEIGHT = V_CELLS * CELL_SIZE;
-let canvas = document.createElement('canvas');
+const UPDATES_PER_SECOND = 10;
+const V_SPEED_CELLS_PER_SECOND = 2;
+const V_SPEED_CELLS_PER_UPDATE = V_SPEED_CELLS_PER_SECOND / UPDATES_PER_SECOND;
+const board = [];
+for (let i = 0; i < H_CELLS; i++) {
+    let column = [];
+    for (let j = 0; j < V_CELLS; j++) {
+        column.push(false);
+    }
+    board.push(column);
+}
+
+const canvas = document.createElement('canvas');
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 canvas.style = "border:1px solid #000000;"
 document.body.insertBefore(canvas, document.body.childNodes[0]);
-let ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 let k;
 window.addEventListener('keydown', e => {
     k = e.keyCode;
     e.preventDefault();
 }, false);
-
 
 function player(x, y, color) {
     this.x = x;
@@ -24,47 +35,53 @@ function player(x, y, color) {
 
     this.draw = context => {
         context.fillStyle = this.color;
-        context.fillRect(this.x * CELL_SIZE, this.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        context.fillRect(this.x * CELL_SIZE, Math.floor(this.y) * CELL_SIZE, CELL_SIZE, CELL_SIZE);
     }
 
-    this.outside = (x, y) => x < 0 || y < 0 || x >= H_CELLS || y >= V_CELLS;
-
-    this.move = (deltaX, deltaY) => {
-        if (!this.outside(this.x + deltaX, this.y + deltaY)) {
-            this.x += deltaX;
-            this.y += deltaY;
+    this.moveH = (deltaX) => {
+        let newX = this.x + deltaX;
+        if (newX >= 0 && newX < H_CELLS && !board[newX][Math.floor(this.y)]) {
+            this.x = newX;
         }
-    }
+    };
 
-    this.moveUp = () => this.move(0, -1);
-    this.moveDown = () => this.move(0, 1);
-    this.moveLeft = () => this.move(-1, 0);
-    this.moveRight = () => this.move(1, 0);
 }
 
-let p = new player(0, 0, 'red');
+let p = new player(Math.round(H_CELLS / 2), 0, 'red');
 
 function movePieces() {
     switch(k) {
         case 37:
-            p.moveLeft();
-            break;
-        case 38:
-            p.moveUp();
+            p.moveH(-1);
             break;
         case 39:
-            p.moveRight();
-            break;
-        case 40:
-            p.moveDown();
+            p.moveH(1);
             break;
     }
     k = 0;
+
+    p.y += V_SPEED_CELLS_PER_UPDATE;
+    let f = Math.floor(p.y);
+    if (f >= V_CELLS || board[p.x][f]) {
+        board[p.x][f-1] = true;
+        p = new player(Math.round(H_CELLS / 2), 0, 'red');
+    } 
 }
 
 function drawScene() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    for (let i = 0; i < H_CELLS; i++) {
+        for (let j = 0; j < V_CELLS; j++) {
+            if (board[i][j]) {
+                ctx.fillStyle = 'blue';
+                ctx.fillRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            }
+        }
+    }
+
     p.draw(ctx);
+
 }
 
 function gameLoop() {
@@ -72,4 +89,4 @@ function gameLoop() {
     drawScene();
 }
 
-setInterval(gameLoop, 100);
+setInterval(gameLoop, 1000 / UPDATES_PER_SECOND);
